beecb045a990160c87c3eae08d6fa5e6d6a93089
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-06-20 17:44:42 -0700

    Time: 191 ms (40.26%), Space: 24 MB (99.44%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        Notes
+        Add INDICES of temperatures to a stack (Allows for us to understand the corresponding index position in res later)
+        Before adding, check if new temperature (at index) is greater than old temperatures (by indices).
+        While it is, pop these values and add the difference in their indices to their corresponding index position in res.
+        At the end of iteration through temperatures, unpopped temperatures will have their corresponding indices in res set to
+        zero.
+        """
+
+        stack = [] #Holds indices of temperatures
+        res = [0] * len(temperatures) #Holds differences in indices
+
+        for i, temp in enumerate(temperatures):
+            while len(stack) != 0 and temperatures[stack[-1]] < temp:
+                res[stack[-1]] = (i - stack[-1])
+                stack.pop()
+            stack.append(i)
+
+        for index in stack:
+            res[index] = 0
+        
+        return res
+
+
+
+        
