e3dbd38d0441dde30f6a10b47db303ce33145796
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-06-24 17:58:41 -0700

    Time: 23 ms (78.96%), Space: 12.9 MB (10.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,16 +3,20 @@ class Solution(object):
         if len(s1) > len(s2):
             return False
         
-        s1_count, s2_count = [0] * 26, [0] * 26
-
+        s1_hash = [0] * 26
+        s2_hash = [0] * 26
+        
+        #Populate s1_hash
         for i in range(len(s1)):
-            s1_count[ord(s1[i]) - ord('a')] += 1
-            s2_count[ord(s2[i]) - ord('a')] += 1
+            s1_hash[ord(s1[i]) - ord('a')] += 1
+            s2_hash[ord(s2[i])- ord('a')] += 1
         
-        for i in range(len(s1), len(s2)):
-            if s1_count == s2_count:
+        for j in range(len(s1), len(s2)):
+            if s1_hash == s2_hash:
                 return True
-            s2_count[ord(s2[i - len(s1)]) - ord('a')] -= 1
-            s2_count[ord(s2[i]) - ord('a')] += 1
-        
-        return s1_count == s2_count
+            
+            s2_hash[ord(s2[j - len(s1)]) - ord('a')] -= 1
+            s2_hash[ord(s2[j]) - ord('a')] += 1
+
+        return s1_hash == s2_hash
+
