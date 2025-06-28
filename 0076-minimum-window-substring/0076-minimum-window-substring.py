89bd4d193f8b0400381fe8b92a1624e983bad9f0
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-06-28 03:48:29 -0700

    Time: 74 ms (90.24%), Space: 15.8 MB (13.87%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        if t == "":
+            return ""
+
+        t_map, window = {}, {}
+        res, resLen = [-1, -1], float('inf')
+        
+        for c in t:
+            t_map[c] = 1 + t_map.get(c, 0)
+
+        have, need = 0, len(t_map)
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            
+            if c in t_map and window[c] == t_map[c]:
+                have += 1
+            
+            while have == need:
+                length = (r - l + 1)
+                if length < resLen:
+                    res = [l, r]
+                    resLen = length
+
+                left = s[l]
+                window[left] -= 1
+
+                if left in t_map and window[left] < t_map[left]:
+                    have -= 1
+                
+                l += 1
+            
+        l, r = res
+        return s[l : r + 1] if resLen != float('inf') else ""
+
+        
+
+        
