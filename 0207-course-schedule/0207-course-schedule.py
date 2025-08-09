da032655bcff76f2e962bcfc0c78fe1eb4cd987e
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-08-09 02:45:14 -0700

    Time: 8 ms (23.64%), Space: 19.6 MB (21.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -24,7 +24,7 @@ class Solution:
 
             return True
 
-        for crs in range(numCourses):
+        for crs, _ in prerequisites:
             if not dfs(crs):
                 return False
         return True
