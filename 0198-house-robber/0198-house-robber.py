d88618c7b5558b63fcd42ea5bad81a6e9e734ff7
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-12-29 18:15:39 -0800

    Time: 0 ms (100%), Space: 17.4 MB (89.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0 #rob1 stores max loot two houses ago, rob2 stores max loot one house ago
-        
-        for n in nums:
-            rob1, rob2 = rob2, max(n + rob1, rob2)
+        rob1, rob2 = 0, 0 # max money 1 house ago, max money 2 houses ago
 
+        for n in nums:
+            rob1, rob2 = rob2, max(n + rob1, rob2) # advancing; either decide to rob this house or forgo b/c the profits of robbing the house between them is larger
+        
         return rob2
