73aaa1964e4da3b59e14652f8dd0045e12c69566
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2025-12-31 03:57:08 -0800

    Time: 249 ms (50.07%), Space: 19.8 MB (92.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # idea is to use dfs and mark land as visited (part of an island) as you get to new land    
-        # O(N^2)
-
         ROWS, COLS = len(grid), len(grid[0])
-        visited = set()
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         islands = 0
 
-        def dfs(r, c): # DFS algo
-            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == "0" or (r, c) in visited: # base cases
+        def dfs(r, c):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == "0":
                 return
-            visited.add((r, c)) # mark land as visited
-            
+
+            grid[r][c] = "0" # mark as visited
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for dr, dc in directions:
-                dfs(r + dr, c + dc) # recursively check neighboring islands
+                dfs(r + dr, c + dc)
         
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) not in visited and grid[r][c] == "1":
-                    dfs(r, c) # mark neighboring land as part of same island
+                if grid[r][c] == "1":
+                    dfs(r, c)
                     islands += 1
         
         return islands
