c6dac91383ac35032924c07a700d496ae2e9798d
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-01-01 03:10:31 -0800

    Time: 0 ms (100%), Space: 17.3 MB (85.77%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,17 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def invertTree(self, root):
-        if root == None:
-            return
-        
-        root.left, root.right = root.right, root.left
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node):
+            if node == None:
+                return
+            node.left, node.right = node.right, node.left
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+            dfs(node.left)
+            dfs(node.right)
+            
+            return node
 
-        return root
+        return dfs(root)
