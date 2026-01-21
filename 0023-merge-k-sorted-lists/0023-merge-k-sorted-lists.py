6ae0c338cbaf5d9a88c73a8ec2c58f9e782d816e
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-01-20 18:35:47 -0800

    Time: 11 ms (61.03%), Space: 22.7 MB (22.93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,42 +1,39 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        if not lists or len(lists) == 0:
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
             return None
-        
-        while len(lists) > 1:
-            mergedLists = []
+
+        while(len(lists) != 1):
+            curLists = []
+
             for i in range(0, len(lists), 2):
-                l1, l2 = lists[i], lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
-        
+                curLists.append(self.mergeLists(lists[i], lists[i + 1] if (i + 1) < len(lists) else None))
+
+            lists = curLists
+
         return lists[0]
+        
+    def mergeLists(self, list1, list2):
+        dummy = node = ListNode()
 
-            
-
-    
-    def mergeList(self, l1, l2):
-        dummy = ListNode()
-        cur = dummy
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = l1
-                l1 = l1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
             else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
-            
-        if l1:
-            cur.next = l1
-        if l2:
-            cur.next = l2
+                node.next = list2
+                list2 = list2.next
+
+            node = node.next
+
+        if list1:
+            node.next = list1
+        if list2:
+            node.next = list2
 
         return dummy.next
-        
