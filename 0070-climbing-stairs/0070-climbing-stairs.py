f3e70d9736700cc8629c13bca82077317ee59fc6
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-06-29 22:42:32 -0700

    Time: 0 ms (100%), Space: 12.4 MB (20.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        # bottom-up DP
-        # ways(n) = ways(n - 1) + ways(n - 2)
+class Solution(object):
+    def climbStairs(self, n):
+        #top-down dp, number of ways to get to step i is number of ways to get to step (i-1) + number of ways to get to step (i-2)
 
-        one, two = 1, 1 # init
+        step1 = 1 
+        step2 = 1
 
-        for i in range(n - 1):
-            one, two = one + two, one # step
+        for i in range(n):
+            step1, step2 = step2, step2 + step1
 
-        return one
+        return step1
