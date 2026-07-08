f278beac25589d26623cda93f055aa1fff673373
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-07 22:21:46 -0700

    Time: 515 ms (58.63%), Space: 19.3 MB (98.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1) 
-        dp[0] = 0
+        # bottom-up DP: tabulation
+        # the key idea is that the minimum amount of coins needed to make (amount) is the minimum amount 
+        # to make (amount-coin) for some coin and adding 1 to that amount
+
+        # thus, we can get a good solution by checking each combination of coins, and optimize this solution by caching the minimum amount of coins it takes to make some number, which will be revisited across different combinations
+
+        dp = [(amount + 1)] * (amount + 1) # the max coins it can take to create amount is amount (b/c the minimum coin value is 1)
+        dp[0] = 0 # base case: it takes 0 coins to make 0
 
         for a in range(1, amount + 1):
             for c in coins:
-                if (a-c) >= 0: #if a combination with the current coin and amount is possible
-                    dp[a] = min(dp[a], 1 + dp[a - c]) #either current value, or coin + dp[amount]
-
-        return dp[amount] if dp[amount] != float('inf') else -1
+                if a - c >= 0:
+                    # either you HAVE the optimal amount, or you can create a new one by choosing a different combination of coins
+                    dp[a] = min(dp[a], 1 + dp[a - c]) 
+        
+        return dp[amount] if dp[amount] != (amount + 1) else -1 # if dp[amount] == amount + 1 they're no way to reach that value
