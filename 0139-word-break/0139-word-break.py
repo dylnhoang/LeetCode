040d7c46f8d90298ac69aec9d6de316f2c0fecf5
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-12 02:58:16 -0700

    Time: 79 ms (5.59%), Space: 12.4 MB (41.87%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,22 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
-        # idea: the validity of a word is defined by the validity of the following string
-        # i.e. we should start from the end of the string to check for words first, because if we can't find words at the end of the string, there's no words in the beginning
+        # idea: the validity of the string depends on the validity of the various substrings within it
+        # so, we can use a top-down DP approach with memoization, caching whether it is possible to form a valid string at a certain point
+        # we go backwards and mark whether it's possible to form a valid string at position i, because at i we need to know if the points at (i + 1) onward are valid
+        #if we can get to the end from 0, we have a valid string
 
-        dp = [False] * (len(s) + 1) 
-        dp[len(s)] = True # if we reach the end of the string (from the beginning), we know we our string is valid
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True # base case: if we reach the end of the string, we're done
 
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i : i + len(w)] in wordDict:
-                    dp[i] = dp[i + len(w)] # refer to idea
+                cand = i + len(w)
+                if cand <= len(s) and s[i : cand] in wordDict:
+                    dp[i] = dp[cand] # if we can create a word at i, then the string is valid iff the we can create a different set of words at i + len(w)
+                    print(dp[i : cand])
                     if dp[i]:
-                        break
-        
+                        break # we only need to find one valid string at i
+
         return dp[0]
-        
+
+
