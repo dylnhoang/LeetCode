0a79c821b7b52785c8d9baa2aa50dfb0c8b4343c
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-12 03:07:53 -0700

    Time: 2272 ms (30.49%), Space: 12.8 MB (10.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
-        n = len(nums)
-        dp = [1] * (n + 1)
+        # idea: the LIS at point i in the list is determined by the LIS at point j (where nums[j] is a value greater than nums[i], WLOG)
+        # brute-force idea would be to use recursion, but this would be a O(2^n) soln; clearly not optimal
+        # we can optimize using a top-down DP approach, where we memoize the LIS at point i for later usage at point (i - 1) WLOG
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i + 1, n):
+        LIS = [1] * (len(nums) + 1)
+        
+        for i in range(len(nums) - 1, -1, -1): # go backwards through nums
+            for j in range(i + 1, len(nums)): # check every value in front of nums[i]
                 if nums[j] > nums[i]:
-                    dp[i] = max(1 + dp[j], dp[i])
+                    LIS[i] = max(LIS[i], 1 + LIS[j]) # either the stored value is already the max, or we can form a better LIS at point i by prepending to a LIS at point j
 
-        return max(dp)
+        return max(LIS)
+
         
