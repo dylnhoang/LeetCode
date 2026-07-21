3b693659007ef3b61f321424d414563e8d8451e4
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-21 03:40:13 -0700

    Time: 79 ms (64.13%), Space: 13.3 MB (65.06%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,18 +6,17 @@
 #         self.right = right
 class Solution(object):
     def isSubtree(self, root, subRoot):
-        if not subRoot:
-            return True
         if not root:
             return False
+        if not subRoot:
+            return True
         
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) or self.isSameTree(root, subRoot)
+        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
     def isSameTree(self, p, q):
         if not p and not q:
             return True
-        elif not p or not q or p.val != q.val:
+        if not p or not q or p.val != q.val:
             return False
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
