0b7b1c7476d94e0e5addaa9e562a997f1888a3c1
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-21 23:05:45 -0700

    Time: 1 ms (46.2%), Space: 21 MB (21.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,12 +7,13 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         return self.valid(float('-inf'), float('inf'), root)
-    
-    def valid(self, min, max, node):
-        if not node:
-            return True
 
-        if not (node.val < max and node.val > min):
+    # we implement a helper method that keeps track of the current min, current max, and the node we want to check the validity of
+    # if we go left, we change the current max value; if we go right, we change the current min value
+    def valid(self, min, max, node):
+        if not node: 
+            return True
+        elif node.val <= min or node.val >= max:
             return False
-        
-        return self.valid(node.val, max, node.right) and self.valid(min, node.val, node.left)
+        else:
+            return self.valid(min, node.val, node.left) and self.valid(node.val, max, node.right)
