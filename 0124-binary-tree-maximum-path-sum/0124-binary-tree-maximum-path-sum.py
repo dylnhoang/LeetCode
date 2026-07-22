5bf2dabce4fd6a2ea8b571052d26aa5de42ceb53
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-07-21 23:52:54 -0700

    Time: 4 ms (94.27%), Space: 23.7 MB (91.01%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -1,25 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxPathSum(self, root):
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # we use an array to store our result so we can access it within our dfs method easily
         res = [root.val]
 
         def dfs(node):
             if not node:
                 return 0
-            
-            leftMax, rightMax = max(0, dfs(node.left)), max(0, dfs(node.right))
 
-            res[0] = max(res[0], leftMax + rightMax + node.val)
+            leftMax = max(0, dfs(node.left)) # determine the path from the left node that yields the max value WITHOUT splits
+            rightMax = max(0, dfs(node.right)) # determine the path from the left node that yields the max value WITHOUT splits
 
-            return node.val + max(leftMax, rightMax)
+            res[0] = max(res[0], leftMax + node.val + rightMax) # check if you can yield a max value by splitting at the current node
+            return node.val + max(leftMax, rightMax) # return the max value you can get at this node (you must include the node) without splitting 
 
         dfs(root)
-
         return res[0]
-
-        
