20f07367c8b48512605b197bfb86fa0321377100
dylnhoang <167364396+dylnhoang@users.noreply.github.com>
2026-08-04 18:27:03 -0700

    Time: 71 ms (73.76%), Space: 49 MB (55.69%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,34 +1,16 @@
-class Solution(object):
-    def eraseOverlapIntervals(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: int
-        """
-        
-        # we start by sorting the array of intervals so that we can handle them sequentially
-        # this also means there is only one condition for overlap: when a further interval starts before a previous one ends
-
-        intervals.sort(key = lambda x: x[0])
-
-        # thinking about this, if we want to remove an interval when there's a collision, is it better to remove the one with the later or earlier end point?
-        # it will always be better to remove the one with the later end point, as this leaves a greater range of possibilities for start points that the rest of the intervals can have, i.e. minimizing removals
-        # thus, the start point is irrelevant to this problem; we can just look at the end points of each interval and solve this problem efficiently 
-
-        # so, we do this problem by keeping track of the previous endpoint, and changing this as we progress
-        # if there's a collision, we change prevEnd to the min endpoint between the two colliding intervals
-
-        count = 0
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x: x[0]) # sort intervals by ascending start
+        res = 0
         prevEnd = intervals[0][1]
 
-
-        for i in range(1, len(intervals)):
-            curInterval = intervals[i]
-
-            # check for collision, i.e. check if the current intervals starts before prevEnd
-            if curInterval[0] < prevEnd:
-                prevEnd = min(prevEnd, curInterval[1])
-                count += 1
+        for start, end in intervals[1:]:
+            # non-overlapping
+            if start >= prevEnd:
+                prevEnd = end
+            # overlapping
             else:
-                prevEnd = curInterval[1]
-
-        return count
+                res += 1
+                prevEnd = min(end, prevEnd) # idea: keeping the point with the earlier end-time is optimal b/c it leaves more room for later intervals to fill (think about number lines)
+            
+        return res
